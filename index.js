27d8568fbbcd684a1dde9bbeae7bb574bd9d8747
adityakakarla <85147239+adityakakarla@users.noreply.github.com>
2021-11-01 11:09:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var color_button = document.getElementById("color_button");
 var color = document.querySelector(".color");
 
-inputGuess.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("color_button").click();
-    }
-});
-
 color_button.addEventListener("click", function (){
     var red = Math.floor(Math.random()*255)
     var blue = Math.floor(Math.random()*255)
